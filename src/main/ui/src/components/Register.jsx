167cd0f018aafafbe36e75839a08023324dc135c
Mango <greatdogsite@gmail.com>
2023-10-30 15:13:07 -0500

    learning a lot of react concepts

--- src/main/ui/src/components/Register.jsx
+++ src/main/ui/src/components/Register.jsx
@@ -1,11 +1,13 @@
 import { useRef, useState, useEffect} from "react";
+import { Link } from "react-router-dom";
 
 // const USER_REGEX = /^[A-z][A-z0-9-_]{3,23}$/; // example: Mar_k6
 const USER_REGEX = /^[A-z0-9-_]{3,23}$/; // example: mark_6
-const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
+const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,50}$/;
 
 function Register(){
-    const useRef = useRef(); // sets focus on user input when function loads
+    const userRef = useRef(); // sets focus on user input when function loads
+    const errRef = useRef();
 
     // username
     const [user, setUser] = useState("");
@@ -32,17 +34,118 @@ function Register(){
     const [success, setSuccess] = useState(false);
 
     useEffect(() => {
-        useRef.current.focus();
+        userRef.current.focus(); // sets focus when component loads on username input
     }, []) // zero dependency array
 
-    useEffect(() => {
-        const result = PWD_REGEX.test(user);
+    useEffect(() => { // validate user
+        const result = USER_REGEX.test(user);
+        setValidUser(result); // set valid state to true or false
     }, [user])
+
+    useEffect(() => { // validate passwords
+        const result = PWD_REGEX.test(password);
+        setValidPassword(result); // set valid state to true or false
+        const matching = password === matchPassword;
+        setValidMatchPassword(matching);
+    }, [password, matchPassword])
+
+    useEffect(() => {
+        setErrorMessage('');
+    }, [user, password, matchPassword]) // after error message is read, and one of these three changes, error message will clear
+
+    const handleSubmit = async (event) => {
+        event.preventDefault(); 
+
+        // possibly prevent a javascript hack from enabling the submit button and submitting bad data
+        if (!USER_REGEX(user) || !PWD_REGEX(password)){
+            setErrorMessage("Invalid Entry");
+            return;
+        }
+
+
+    }
     
     return(
-        <div>
+        // create ternary fragment
+        <> 
+        {success ? 
+        (
+            <section>
+                <h1>Sign Up Successful!</h1>
+                <p>
+                    <Link to="/-/login">Log In Here</Link>
+                </p>
 
-        </div>
+            </section>
+
+        ) : (
+            <section>
+                {/* <p ref={errRef}>{errMessage}</p> */}
+                <p ref={errRef}>error</p>
+                <h1>Registration</h1>
+                <form onSubmit = {handleSubmit}>
+                    {/* //htmlFor sets focus on username input when label is clicked */}
+                    <label htmlFor="username">
+                        Username:
+                    </label>        
+                    <input
+                        type = "text"
+                        id = "username"
+                        ref = {userRef}  
+                        onChange = {(e) => setUser(e.target.value)}     
+                        required
+                        onFocus = {() => setUserFocus(true)}
+                        // onBlur is for when user leaves input box
+                        onBlur = {() => setUserFocus(false)}
+                    />
+                    <p className = {userFocus && user && !validUser ? "instructions" : "hide"}>
+                        3 - 50 characters <br />
+                        Must begin with a letter. <br />
+                        Letters, numbers, underscores, hyphens allowed.
+                    </p>
+
+                    <label htmlFor="password">
+                        Password:
+                    </label>        
+                    <input
+                        type = "password"
+                        id = "password"
+                        onChange = {(e) => setPassword(e.target.value)}     
+                        required
+                        onFocus = {() => setPasswordFocus(true)}
+                        onBlur = {() => setPasswordFocus(false)}
+                    />
+                    <p className = {passwordFocus && !validPassword ? "instructions" : "hide"}>
+                        8 - 50 characters <br />
+                        Must contain one uppercase and lowercase letter, a number and a special character <br />
+                        Special characters allowed: !@#$%
+                    </p>
+
+                    <label htmlFor="verifyPassword">
+                        Verify Password:
+                    </label>        
+                    <input
+                        type = "password"
+                        id = "verifyPassword"
+                        onChange = {(e) => setMatchingPassword(e.target.value)}     
+                        required
+                        onFocus = {() => setMatchPasswordFocus(true)}
+                        onBlur = {() => setMatchPasswordFocus(false)}
+                    />
+                    <p className = {matchPasswordFocus && !validMatchPassword ? "instructions" : "hide"}>
+                        Passwords do not match.
+                    </p>
+                    <button disabled = {!validUser || !validPassword || !validMatchPassword ? true : false }>
+                        Sign up
+                    </button>
+                    <p>Already registered? <br />
+                    <Link to="/-/login">Log in</Link>
+                    </p>
+                </form>
+                
+            </section>
+            )}
+        </>
     )
 
 }
